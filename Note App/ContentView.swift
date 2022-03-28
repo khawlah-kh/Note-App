d4a26d1797f9946f53677bceb1a4f4a478c27d04
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-03-28 07:47:46 +0300

    Initial Commit

---
+++ Note App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Note App
+//
+//  Created by khawlah khalid on 28/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Note App/Note_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_AppApp.swift
+//  Note App
+//
+//  Created by khawlah khalid on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Note_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
